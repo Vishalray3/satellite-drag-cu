0fc81729de51d651c8a35d511917b2b1f2a23c6f
Vishalray3 <39605038+Vishalray3@users.noreply.github.com>
2024-07-31 08:55:14 -0600

    Update spire_plots_all.m

--- Density_inversion_methods/Simultaneous_estimation/spire_plots_all.m
+++ Density_inversion_methods/Simultaneous_estimation/spire_plots_all.m
@@ -29,7 +29,8 @@ for ii=1:numel(file_names)
     rho_hasdm_all = [rho_hasdm_all, rho_hasdm_eff];
     rho_nom_all = [rho_nom_all, rho_nom_eff];
     rho_data_all = [rho_data_all, rho_eff];
-    time_rho_all = [time_rho_all, time_rho];
+    time_cal = datetime((jd_init + time_rho)/86400,'convertfrom','juliandate','Format','yyy-MM-dd-hh-mm-ss');
+    time_rho_all = [time_rho_all, time_cal];
 end
 
 %%
@@ -44,30 +45,30 @@ data_rms = rms(data_ratio', "omitnan")';
 
 figure(1)
 subplot(3,1,1)
-plot(time_rho_all(1,:)/3600,rho_hasdm_all(1,:),'k.','LineWidth',1)
+plot(time_rho_all(1,:),rho_hasdm_all(1,:),'k.','LineWidth',1)
 hold on
-plot(time_rho_all(1,:)/3600,rho_nom_all(1,:),'r.','LineWidth',1)
-plot(time_rho_all(1,:)/3600,rho_data_all(1,:),'g.','LineWidth',1)
+plot(time_rho_all(1,:),rho_nom_all(1,:),'r.','LineWidth',1)
+plot(time_rho_all(1,:),rho_data_all(1,:),'g.','LineWidth',1)
 grid on
 title('30-minute arc-length')
 legend('HASDM','MSIS00','Spire EDR')
 set(gca,'FontSize',14)
 
 subplot(3,1,2)
-plot(time_rho_all(2,:)/3600,rho_hasdm_all(2,:),'k.','LineWidth',1)
+plot(time_rho_all(2,:),rho_hasdm_all(2,:),'k.','LineWidth',1)
 hold on
-plot(time_rho_all(2,:)/3600,rho_nom_all(2,:),'r.','LineWidth',1)
-plot(time_rho_all(2,:)/3600,rho_data_all(2,:),'g.','LineWidth',1)
+plot(time_rho_all(2,:),rho_nom_all(2,:),'r.','LineWidth',1)
+plot(time_rho_all(2,:),rho_data_all(2,:),'g.','LineWidth',1)
 grid on
 ylabel('Density ($kg/m^3$)','Interpreter','latex')
 title('60-minute arc-length')
 set(gca,'FontSize',14)
 
 subplot(3,1,3)
-plot(time_rho_all(3,:)/3600,rho_hasdm_all(3,:),'k.','LineWidth',1)
+plot(time_rho_all(3,:),rho_hasdm_all(3,:),'k.','LineWidth',1)
 hold on
-plot(time_rho_all(3,:)/3600,rho_nom_all(3,:),'r.','LineWidth',1)
-plot(time_rho_all(3,:)/3600,rho_data_all(3,:),'g.','LineWidth',1)
+plot(time_rho_all(3,:),rho_nom_all(3,:),'r.','LineWidth',1)
+plot(time_rho_all(3,:),rho_data_all(3,:),'g.','LineWidth',1)
 grid on
 ylabel('Density ($kg/m^3$)','Interpreter','latex')
 title('90-minute arc-length')
@@ -76,27 +77,27 @@ saveas(gcf, fullfile(fig_path, strcat('spire_', year, '_', sat_id, '_density_edr
     
 figure(2)
 subplot(3,1,1)
-plot(time_rho_all(1,:)/3600, nom_ratio(1,:),'k.','LineWidth',1)
+plot(time_rho_all(1,:), nom_ratio(1,:),'k.','LineWidth',1)
 hold on
-plot(time_rho_all(1,:)/3600, data_ratio(1,:),'r.','LineWidth',1)
+plot(time_rho_all(1,:), data_ratio(1,:),'r.','LineWidth',1)
 grid on
 title('30-minute arc-length')
 legend(sprintf('MSIS00/HASDM, mean=%0.2f, rms=%0.2f', nom_mean(1), nom_rms(1)),sprintf('Spire-EDR/HASDM, mean=%0.2f, rms=%0.2f', data_mean(1), data_rms(1)))
 set(gca,'FontSize',14)
 
 subplot(3,1,2)
-plot(time_rho_all(2,:)/3600, nom_ratio(2,:),'k.','LineWidth',1)
+plot(time_rho_all(2,:), nom_ratio(2,:),'k.','LineWidth',1)
 hold on
-plot(time_rho_all(2,:)/3600, data_ratio(2,:),'r.','LineWidth',1)
+plot(time_rho_all(2,:), data_ratio(2,:),'r.','LineWidth',1)
 grid on
 title('60-minute arc-length')
 legend(sprintf('MSIS00/HASDM, mean=%0.2f, rms=%0.2f', nom_mean(2), nom_rms(2)),sprintf('Spire-EDR/HASDM, mean=%0.2f, rms=%0.2f', data_mean(2), data_rms(2)))
 set(gca,'FontSize',14)
 
 subplot(3,1,3)
-plot(time_rho_all(3,:)/3600, nom_ratio(3,:),'k.','LineWidth',1)
+plot(time_rho_all(3,:), nom_ratio(3,:),'k.','LineWidth',1)
 hold on
-plot(time_rho_all(3,:)/3600, data_ratio(3,:),'r.','LineWidth',1)
+plot(time_rho_all(3,:), data_ratio(3,:),'r.','LineWidth',1)
 grid on
 title('90-minute arc-length')
 legend('MSIS00/HASDM','Spire-EDR/HASDM')
